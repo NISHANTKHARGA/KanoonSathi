9f2b033e0e734dbe5b0ecd7ae0a7a8597b0f64ab
Alishiba Adhikari <mealishiba@gmail.com>
2026-05-13 07:25:35 +0545

    Completed database setup

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('client', 'Client'),
+        ('lawyer', 'Lawyer'),
+        ('admin', 'Admin'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.user.username
+    
+class Lawyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)
+    experience_years = models.IntegerField()
+    license_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+class Appointment(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='client_appointments')
+    lawyer = models.ForeignKey(Lawyer, on_delete=models.CASCADE, related_name='lawyer_appointments')
+
+    date = models.DateField()
+    time = models.TimeField()
+    STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('confirmed', 'Confirmed'),
+    ('rejected', 'Rejected'),
+]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.client.username} -> {self.lawyer.user.username}"
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from .models import UserProfile
 # Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        role = request.POST.get('role')
+
+        user = User.objects.create_user(username=username, password=password)
+        UserProfile.objects.create(user=user, role=role)
+
+        return redirect('/login/')
+
+    return render(request, 'signup.html')
